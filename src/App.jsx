b286c348e1ab0c2f9f4beee8d78ca49875b1eec3
Ayush <140370865+Ayyayush@users.noreply.github.com>
2025-07-05 15:29:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar"
+import React from "react"
+import { Route, Routes, useLocation } from "react-router-dom"  
+import Home from "./pages/Home";
+function App() {
+ 
+const isOwnerPath = useLocation().pathname.includes("/owner");
+  return (
+    <div>
+    { !isOwnerPath && < Navbar />}
+    <div className="min-h-[70vh]">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        </Routes>
+
+    </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
+      <p>The Ultimate Hotel Experience </p>
+      <p>Unparalled Luxury and Comfort await at the worl's most exlucsive hotels and resorts. Start you journey today</p>
+    </div>
+  )
+}
+
+export default Hero;
