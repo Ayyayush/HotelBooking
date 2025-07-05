1721c50147047d11fa7c640e8ebfcea3bef719a6
Ayush <140370865+Ayyayush@users.noreply.github.com>
2025-07-05 16:33:09 +0530

    Client

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import {assets} from '../assets/assets';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate,useLocation } from 'react-router-dom';
+import { useClerk, useUser,UserButton } from '@clerk/clerk-react';
+
+
+const BookIcon = ()=>(
+    <svg className="w-4 h-4 text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" >
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4" />
+</svg>
+)
 
 
 const Navbar = () => {
@@ -15,6 +23,12 @@ const Navbar = () => {
 
     const [isScrolled, setIsScrolled] = React.useState(false);
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+    const {openSignIn} = useClerk();
+    const {user}=useUser();
+    const navigate=useNavigate();
+    const Location= useLocation();
+   
 
     React.useEffect(() => {
         const handleScroll = () => {
@@ -49,11 +63,20 @@ const Navbar = () => {
 
                 <div className="hidden md:flex items-center gap-4">
                     <img src={assets.searchIcon} alt="search" className={`${isScrolled && "invert"} h-7 duration-500`} />
-    
-                    <button className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
+
+                    {user ?
+                      (<UserButton>
+                        <userButton.MenuItems>
+                            <userButton.Action label="My Bookings" labelIcon={<BookIcon />} onClick={() => navigate('/my-bookings')} />
+                            </userButton.MenuItems> 
+                         </UserButton>)
+                    :
+                    (   <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full ml-4 transition-all duration-500">
                         Login
                     </button>
-                </div>
+                )
+                    }
+                 </div>
 
                 {/* Mobile Menu Button */}
                 <div className="flex items-center gap-3 md:hidden">
@@ -76,7 +99,7 @@ const Navbar = () => {
                         DashBoard
                     </button>
 
-                    <button className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                    <button onClick={openSignIn} className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
                         Login
                     </button>
                 </div>
